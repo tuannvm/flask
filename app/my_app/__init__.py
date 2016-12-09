01d81ec7daa6cf36b20346833b615231fe1fe59a
Tuan <tuanpro.itman@gmail.com>
2016-12-09 15:52:21 +0700

    chap 2 cont

--- app/my_app/__init__.py
+++ app/my_app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from my_app.hello.views import hello
+from my_app.product.views import product
 
 app = Flask(__name__)
 app.register_blueprint(hello)
+app.register_blueprint(product)
